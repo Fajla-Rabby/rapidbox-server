82330bc9ecd1e1899d9297d52c00a94052ca2374
Fajla-Rabby <fajla.rabby008@gmail.com>
2023-03-30 15:41:15 +0600

    env

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kccqe.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://rapidbox:Ohf1eUsz5SAuD3Hl@cluster0.kccqe.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
